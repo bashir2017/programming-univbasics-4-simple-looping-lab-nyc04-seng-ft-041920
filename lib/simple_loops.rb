d0ae45fde68f2aa68d2248c2659da402875bc98c
Bashir <bashiralhanshali@yahoo.com>
2020-04-23 13:32:57 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,23 @@
 # Write your methods here
+
+def loop_message_five_times(str)
+  x = 0 
+  while x < 5 do 
+    puts str
+  end 
+end
+
+def loop_message_n_times(str, num)
+  x = 0 
+  while x < num do 
+    puts str 
+  end 
+end 
+
+def output_array(array)
+  x = 0 
+  while array[x] do 
+    puts array[x]
+    x = x+1 
+  end 
+end 
